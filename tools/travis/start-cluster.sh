e3e1956c42e5b40907968b977a394633c601cebc
Nick Mitchell <nickm@us.ibm.com>
2018-11-26 11:25:07 -0500

    travis kube build

---
+++ tools/travis/start-cluster.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/apache/incubator-openwhisk-deploy-kube && \
+    cd incubator-openwhisk-deploy-kube && \
+    ./tools/travis/start-kubeadm-dind.sh && \
+    ./tools/travis/build-helm.sh
